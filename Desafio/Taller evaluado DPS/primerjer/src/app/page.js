b4b2ed905d67a2ae151e30e1c1062820baf0a7bd
Cgonzalez1930 <125714110+Cgonzalez1930@users.noreply.github.com>
2024-03-01 15:39:17 -0600

    first commit

---
+++ Desafio/Taller evaluado DPS/primerjer/src/app/page.js
@@ -0,0 +1,93 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Calculo from "@/component/Calculo";
+
+export default function Home() {
+  // Estado para almacenar los movimientos
+  const [movimientos, setMovimientos] = useState([]);
+  const [tipo, setTipo] = useState("Ingreso");
+  const [cantidad, setCantidad] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+
+  // Función para registrar un nuevo movimiento
+  const registrarMovimiento = () => {
+    const nuevoMovimiento = {
+      tipo,
+      cantidad: parseFloat(cantidad),
+      categoria,
+      fecha,
+      descripcion,
+    };
+    setMovimientos([...movimientos, nuevoMovimiento]);
+    // Limpiar los campos después de registrar el movimiento
+    setTipo("Ingreso");
+    setCantidad("");
+    setCategoria("");
+    setFecha("");
+    setDescripcion("");
+  };
+
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Presupuesto Mensual</h1>
+      <div className={styles.formulario}>
+        <div className={styles.campos}>
+          <label className={styles.text}>Tipo:</label>
+          <select
+            className={styles.inputField}
+            value={tipo}
+            onChange={(e) => setTipo(e.target.value)}
+          >
+            <option value="Ingreso">Ingreso</option>
+            <option value="Gasto">Gasto</option>
+          </select>
+        </div>
+        <div className={styles.campos}>
+          <label className={styles.text}>Cantidad:</label>
+          <input
+            className={styles.inputField}
+            type="number"
+            value={cantidad}
+            onChange={(e) => setCantidad(e.target.value)}
+          />
+        </div>
+        
+          <div className={styles.campos}>
+            <label className={styles.text}>Categoría:</label>
+            <input
+              className={styles.inputField}
+              type="text"
+              value={categoria}
+              onChange={(e) => setCategoria(e.target.value)}
+            />
+          </div>
+        
+        <div className={styles.campos}>
+          <label className={styles.text}>Fecha:</label>
+          <input
+            className={styles.inputField}
+            type="date"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
+          />
+        </div>
+        <div className={styles.campos}>
+          <label className={styles.text}>Descripción:</label>
+          <input
+            className={styles.inputField}
+            type="text"
+            value={descripcion}
+            onChange={(e) => setDescripcion(e.target.value)}
+          />
+        </div>
+        <button className={styles.button} onClick={registrarMovimiento}>
+          Registrar
+        </button>
+      </div>
+      <Calculo movimientos={movimientos} />
+    </main>
+  );
+}
